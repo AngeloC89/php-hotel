bff1c81c061d63a45252a0fbb16c21437585ad18
AngeloC89 <Angelociulla89@gmail.com>
2024-05-03 18:44:26 +0200

    I done login... added control on auth.php and index.php

--- index.php
+++ index.php
@@ -1,18 +1,22 @@
-//<?php
-//include __DIR__ ."/Controllers/hotelsMethods.php";
-//$template = printTable($data);
-////var_dump($data);
-//
-//
-//include __DIR__ . "/Views/header.php";
-//?>
-
-//<?php
-//include __DIR__ . "/Views/main.php";
-//?> 
-
-
-
 <?php
-header("location: login.php");
+
+// Controllo se l'utente ha effettuato il login
+session_start();
+if (!isset($_SESSION['userId'])) {
+  session_destroy();
+  header('Location: login.php');
+  die();
+};
+
+
+include __DIR__ ."/Controllers/hotelsMethods.php";
+$template = printTable($data);
+//var_dump($data);
+
+
+include __DIR__ . "/Views/header.php";
 ?>
+<?php
+include __DIR__ . "/Views/main.php";
+?> 
+
